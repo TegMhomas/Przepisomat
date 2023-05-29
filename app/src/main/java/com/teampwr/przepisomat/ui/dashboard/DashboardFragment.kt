1c942dedc58f6561d467fbb135b340de241c305d
MrXenic <mrxenic@gmail.com>
2023-05-29 18:52:16 +0200

    added recycler view to fragment

--- app/src/main/java/com/teampwr/przepisomat/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/teampwr/przepisomat/ui/dashboard/DashboardFragment.kt
@@ -7,12 +7,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.teampwr.przepisomat.R
+import com.teampwr.przepisomat.RecipeAdapter
+import com.teampwr.przepisomat.RecipesActivity
 import com.teampwr.przepisomat.databinding.FragmentDashboardBinding
+import com.teampwr.przepisomat.model.Recipe
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecipeAdapter
+    private lateinit var layoutManager: LinearLayoutManager
+    private val db = FirebaseFirestore.getInstance()
+    private val przepisyRef = db.collection("przepisy")
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -32,6 +43,32 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        recyclerView = binding.recyclerView
+        layoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+        val recipes = ArrayList<Recipe>()
+
+
+        przepisyRef.get()
+                .addOnSuccessListener { documents ->
+
+                    for (document in documents) {
+                        val zdjecie = document.getLong("zdjecie")
+                        val nazwa = document.getString("nazwa")
+                        val kategoria = document.getString("kategoria")
+                        val opis = document.getString("opis")
+                        val iloscLajkow = document.getLong("ilosc_lajkow")
+                        System.out.println("KEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKWWWWWWWWWWWWWWWWWWW " + nazwa)
+                        recipes.add(Recipe(R.drawable.baseline_fastfood_24, nazwa, opis, iloscLajkow, kategoria))
+
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+
+                }
+
         return root
     }
 
